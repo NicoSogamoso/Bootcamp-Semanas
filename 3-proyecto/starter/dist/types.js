1530b5618e04d3245c99317f18a21dd128ebd7aa
NicoSogamoso <nicolas.sogamoso10@gmail.com>
2026-08-19 18:58:41 -0500

    Delete 3-proyecto directory

--- 3-proyecto/starter/dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// ============================================================
-// types.ts
-// Aquí viven TODOS los "moldes" (interfaces) que usa la API.
-// Dominio: Club Social -> recurso: members (socios)
-// ============================================================
-Object.defineProperty(exports, "__esModule", { value: true });
